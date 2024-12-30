7ff83ee364671691423234dcb9ac7a74305c7134
NaveenDev-Hub <naveensct@gmail.com>
2024-12-30 15:37:12 -0600

    Add files via upload
    Gender prediction from Indian's and Chinese's calendar

---
+++ gender_predict.py
@@ -0,0 +1,155 @@
+
+chinese_calendar ={
+
+    18:{1:'GIRL',2:'BOY',3:'GIRL',4:'BOY',5:'BOY',6:'BOY',7:'BOY',8:'BOY',9:'BOY',10:'BOY',11:'BOY',12:'BOY'},
+    
+    19:{1:'BOY',2:'GIRL',3:'BOY',4:'GIRL',5:'GIRL',6:'BOY',7:'BOY',8:'GIRL',9:'BOY',10:'BOY',11:'GIRL',12:'GIRL'},
+
+    20:{1:'GIRL',2:'BOY',3:'GIRL',4:'BOY',5:'BOY',6:'BOY',7:'BOY',8:'BOY',9:'BOY',10:'GIRL',11:'BOY',12:'BOY'},
+
+    21:{1:'BOY',2:'GIRL',3:'GIRL',4:'GIRL',5:'GIRL',6:'GIRL',7:'GIRL',8:'GIRL',9:'GIRL',10:'GIRL',11:'GIRL',12:'GIRL'},
+
+    22:{1:'GIRL',2:'BOY',3:'BOY',4:'GIRL',5:'BOY',6:'GIRL',7:'GIRL',8:'BOY',9:'GIRL',10:'GIRL',11:'GIRL',12:'GIRL'},
+
+    23:{1:'BOY',2:'BOY',3:'BOY',4:'GIRL',5:'BOY',6:'BOY',7:'GIRL',8:'GIRL',9:'GIRL',10:'BOY',11:'BOY',12:'GIRL'},
+
+    24:{1:'BOY',2:'GIRL',3:'GIRL',4:'BOY',5:'BOY',6:'GIRL',7:'BOY',8:'GIRL',9:'BOY',10:'BOY',11:'GIRL',12:'BOY'},
+
+    25:{1:'GIRL',2:'BOY',3:'GIRL',4:'BOY',5:'GIRL',6:'BOY',7:'GIRL',8:'BOY',9:'GIRL',10:'BOY',11:'BOY',12:'BOY'},
+
+    26:{1:'BOY',2:'BOY',3:'BOY',4:'BOY',5:'BOY',6:'GIRL',7:'BOY',8:'GIRL',9:'GIRL',10:'BOY',11:'GIRL',12:'GIRL'},
+
+    27:{1:'GIRL',2:'GIRL',3:'BOY',4:'BOY',5:'GIRL',6:'BOY',7:'GIRL',8:'GIRL',9:'BOY',10:'GIRL',11:'BOY',12:'BOY'},
+
+    28:{1:'BOY',2:'BOY',3:'BOY',4:'GIRL',5:'GIRL',6:'BOY',7:'GIRL',8:'BOY',9:'GIRL',10:'GIRL',11:'BOY',12:'GIRL'},
+
+    29:{1:'GIRL',2:'BOY',3:'GIRL',4:'GIRL',5:'BOY',6:'GIRL',7:'GIRL',8:'BOY',9:'GIRL',10:'BOY',11:'GIRL',12:'GIRL'},
+
+    30:{1:'BOY',2:'BOY',3:'GIRL',4:'BOY',5:'GIRL',6:'BOY',7:'BOY',8:'BOY',9:'BOY',10:'BOY',11:'BOY',12:'BOY'},
+
+    31:{1:'BOY',2:'BOY',3:'BOY',4:'BOY',5:'GIRL',6:'GIRL',7:'BOY',8:'GIRL',9:'BOY',10:'GIRL',11:'GIRL',12:'GIRL'},
+
+    32:{1:'BOY',2:'GIRL',3:'GIRL',4:'BOY',5:'GIRL',6:'BOY',7:'BOY',8:'GIRL',9:'BOY',10:'BOY',11:'GIRL',12:'BOY'},
+
+    33:{1:'GIRL',2:'BOY',3:'BOY',4:'GIRL',5:'GIRL',6:'BOY',7:'GIRL',8:'BOY',9:'GIRL',10:'BOY',11:'BOY',12:'GIRL'},
+
+    34:{1:'BOY',2:'BOY',3:'GIRL',4:'GIRL',5:'BOY',6:'GIRL',7:'BOY',8:'BOY',9:'GIRL',10:'BOY',11:'GIRL',12:'GIRL'},
+
+    35:{1:'BOY',2:'GIRL',3:'BOY',4:'GIRL',5:'BOY',6:'GIRL',7:'BOY',8:'GIRL',9:'BOY',10:'BOY',11:'GIRL',12:'BOY'},
+
+    36:{1:'BOY',2:'GIRL',3:'BOY',4:'BOY',5:'BOY',6:'GIRL',7:'BOY',8:'BOY',9:'GIRL',10:'GIRL',11:'GIRL',12:'GIRL'},
+
+    37:{1:'GIRL',2:'GIRL',3:'BOY',4:'GIRL',5:'GIRL',6:'GIRL',7:'BOY',8:'GIRL',9:'GIRL',10:'BOY',11:'BOY',12:'BOY'},
+
+    38:{1:'BOY',2:'BOY',3:'GIRL',4:'GIRL',5:'BOY',6:'GIRL',7:'GIRL',8:'BOY',9:'GIRL',10:'GIRL',11:'BOY',12:'GIRL'},
+
+    39:{1:'GIRL',2:'GIRL',3:'BOY',4:'GIRL',5:'GIRL',6:'GIRL',7:'BOY',8:'GIRL',9:'BOY',10:'BOY',11:'GIRL',12:'BOY'},
+
+    40:{1:'BOY',2:'BOY',3:'BOY',4:'GIRL',5:'BOY',6:'GIRL',7:'BOY',8:'GIRL',9:'BOY',10:'GIRL',11:'GIRL',12:'BOY'},
+
+    41:{1:'GIRL',2:'GIRL',3:'BOY',4:'GIRL',5:'BOY',6:'BOY',7:'GIRL',8:'GIRL',9:'BOY',10:'GIRL',11:'BOY',12:'GIRL'},
+
+    42:{1:'BOY',2:'GIRL',3:'GIRL',4:'BOY',5:'BOY',6:'BOY',7:'BOY',8:'BOY',9:'GIRL',10:'BOY',11:'GIRL',12:'BOY'},
+
+    43:{1:'GIRL',2:'BOY',3:'GIRL',4:'GIRL',5:'BOY',6:'BOY',7:'BOY',8:'GIRL',9:'GIRL',10:'GIRL',11:'BOY',12:'BOY'},
+
+    44:{1:'BOY',2:'GIRL',3:'GIRL',4:'GIRL',5:'BOY',6:'GIRL',7:'BOY',8:'BOY',9:'GIRL',10:'BOY',11:'GIRL',12:'BOY'},
+
+    45:{1:'GIRL',2:'BOY',3:'GIRL',4:'BOY',5:'GIRL',6:'GIRL',7:'BOY',8:'GIRL',9:'BOY',10:'GIRL',11:'BOY',12:'GIRL'}
+}
+
+
+
+indian_calendar = {
+
+    18:{1:'GIRL',2:'BOY',3:'GIRL',4:'BOY',5:'BOY',6:'BOY',7:'BOY',8:'BOY',9:'BOY',10:'GIRL',11:'BOY',12:'BOY'},
+
+    19:{1:'BOY',2:'GIRL',3:'BOY',4:'GIRL',5:'GIRL',6:'BOY',7:'BOY',8:'BOY',9:'BOY',10:'BOY',11:'GIRL',12:'GIRL'},
+
+    20:{1:'GIRL',2:'BOY',3:'GIRL',4:'BOY',5:'GIRL',6:'BOY',7:'BOY',8:'BOY',9:'BOY',10:'GIRL',11:'BOY',12:'BOY'},
+
+    21:{1:'BOY',2:'GIRL',3:'GIRL',4:'GIRL',5:'GIRL',6:'GIRL',7:'GIRL',8:'GIRL',9:'GIRL',10:'GIRL',11:'GIRL',12:'GIRL'},
+
+    22:{1:'GIRL',2:'BOY',3:'BOY',4:'GIRL',5:'BOY',6:'GIRL',7:'GIRL',8:'BOY',9:'GIRL',10:'GIRL',11:'GIRL',12:'GIRL'},
+
+    23:{1:'BOY',2:'BOY',3:'GIRL',4:'BOY',5:'BOY',6:'GIRL',7:'BOY',8:'GIRL',9:'BOY',10:'BOY',11:'BOY',12:'GIRL'},
+
+    24:{1:'BOY',2:'GIRL',3:'BOY',4:'BOY',5:'GIRL',6:'BOY',7:'BOY',8:'GIRL',9:'GIRL',10:'GIRL',11:'GIRL',12:'GIRL'},
+
+    25:{1:'GIRL',2:'BOY',3:'BOY',4:'GIRL',5:'GIRL',6:'BOY',7:'GIRL',8:'BOY',9:'BOY',10:'BOY',11:'BOY',12:'BOY'},
+
+    26:{1:'BOY',2:'GIRL',3:'BOY',4:'GIRL',5:'GIRL',6:'BOY',7:'GIRL',8:'BOY',9:'GIRL',10:'GIRL',11:'GIRL',12:'GIRL'},
+
+    27:{1:'GIRL',2:'BOY',3:'GIRL',4:'BOY',5:'GIRL',6:'GIRL',7:'GIRL',8:'BOY',9:'BOY',10:'BOY',11:'GIRL',12:'BOY'},
+
+    28:{1:'BOY',2:'GIRL',3:'BOY',4:'GIRL',5:'GIRL',6:'GIRL',7:'BOY',8:'BOY',9:'BOY',10:'BOY',11:'GIRL',12:'GIRL'},
+
+    29:{1:'GIRL',2:'BOY',3:'GIRL',4:'GIRL',5:'BOY',6:'BOY',7:'BOY',8:'BOY',9:'BOY',10:'GIRL',11:'GIRL',12:'GIRL'},
+
+    30:{1:'BOY',2:'GIRL',3:'GIRL',4:'GIRL',5:'GIRL',6:'GIRL',7:'GIRL',8:'GIRL',9:'GIRL',10:'GIRL',11:'BOY',12:'BOY'},
+
+    31:{1:'BOY',2:'GIRL',3:'BOY',4:'GIRL',5:'GIRL',6:'GIRL',7:'GIRL',8:'GIRL',9:'GIRL',10:'GIRL',11:'GIRL',12:'BOY'},
+
+    32:{1:'BOY',2:'GIRL',3:'BOY',4:'GIRL',5:'GIRL',6:'GIRL',7:'GIRL',8:'GIRL',9:'GIRL',10:'GIRL',11:'GIRL',12:'BOY'},
+
+    33:{1:'GIRL',2:'BOY',3:'BOY',4:'BOY',5:'GIRL',6:'GIRL',7:'GIRL',8:'BOY',9:'GIRL',10:'GIRL',11:'GIRL',12:'BOY'},
+
+    34:{1:'BOY',2:'GIRL',3:'BOY',4:'GIRL',5:'GIRL',6:'GIRL',7:'GIRL',8:'GIRL',9:'GIRL',10:'GIRL',11:'BOY',12:'BOY'},
+
+    35:{1:'BOY',2:'BOY',3:'GIRL',4:'BOY',5:'GIRL',6:'GIRL',7:'GIRL',8:'BOY',9:'GIRL',10:'GIRL',11:'BOY',12:'BOY'},
+
+    36:{1:'GIRL',2:'BOY',3:'BOY',4:'GIRL',5:'BOY',6:'GIRL',7:'GIRL',8:'GIRL',9:'BOY',10:'BOY',11:'BOY',12:'BOY'},
+
+    37:{1:'BOY',2:'GIRL',3:'BOY',4:'BOY',5:'GIRL',6:'BOY',7:'GIRL',8:'BOY',9:'GIRL',10:'BOY',11:'GIRL',12:'BOY'},
+
+    38:{1:'GIRL',2:'BOY',3:'GIRL',4:'BOY',5:'BOY',6:'GIRL',7:'BOY',8:'GIRL',9:'BOY',10:'GIRL',11:'BOY',12:'GIRL'},
+
+    39:{1:'BOY',2:'GIRL',3:'BOY',4:'BOY',5:'BOY',6:'GIRL',7:'GIRL',8:'BOY',9:'GIRL',10:'BOY',11:'GIRL',12:'GIRL'},
+
+    40:{1:'GIRL',2:'BOY',3:'GIRL',4:'BOY',5:'GIRL',6:'BOY',7:'BOY',8:'GIRL',9:'BOY',10:'GIRL',11:'BOY',12:'GIRL'},
+
+    41:{1:'BOY',2:'GIRL',3:'BOY',4:'GIRL',5:'BOY',6:'GIRL',7:'BOY',8:'BOY',9:'GIRL',10:'BOY',11:'GIRL',12:'BOY'},
+
+    42:{1:'GIRL',2:'BOY',3:'GIRL',4:'BOY',5:'GIRL',6:'BOY',7:'GIRL',8:'BOY',9:'BOY',10:'GIRL',11:'BOY',12:'GIRL'},
+
+    43:{1:'BOY',2:'GIRL',3:'BOY',4:'GIRL',5:'BOY',6:'GIRL',7:'BOY',8:'GIRL',9:'BOY',10:'BOY',11:'BOY',12:'BOY'},
+
+    44:{1:'BOY',2:'BOY',3:'GIRL',4:'BOY',5:'BOY',6:'BOY',7:'GIRL',8:'BOY',9:'GIRL',10:'BOY',11:'GIRL',12:'GIRL'},
+
+    45:{1:'GIRL',2:'BOY',3:'BOY',4:'GIRL',5:'GIRL',6:'GIRL',7:'BOY',8:'GIRL',9:'BOY',10:'GIRL',11:'BOY',12:'BOY'}
+    
+}
+
+
+def get_gender(mother_age,month):
+
+    gender_india = indian_calendar[mother_age][month]
+    gender_china = chinese_calendar[mother_age][month]
+
+    return gender_india,gender_china
+
+while(True):
+    try: 
+        mother_age = int(input("Enter the mother's current age : "))
+        if 18 <= mother_age <= 45:
+            month = int(input("Enter the month : "))
+            if 1 <= month <=12:
+                gender_india,gender_china = get_gender(mother_age,month)
+                print("As per Indian calendar prediction:  ",gender_india)
+                print("As per Chinese calendar prediction: ",gender_china)
+                break  
+            else:
+                raise Exception("Invalid input. Please enter the month between 1 to 12.")  
+        else: 
+            raise Exception("Invalid input. Please enter the age between 18 to 45.")    
+    
+    except ValueError:
+        print("Don't enter any other characters.")
+
+    except Exception as e:
+        print(e)
+
+
+         
+
+
